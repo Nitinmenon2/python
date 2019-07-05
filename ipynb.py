466c82fa7930321b7d44acf4d1ae6ff358693e01
Nitinmenon2 <52524820+Nitinmenon2@users.noreply.github.com>
2019-07-05 15:54:55 +0530

    Add files via upload

---
+++ ipynb.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Markdown Basics
+
+# ## MArkdown basic
+# 
+
+# ###### Markdown
+# 
+
+# ###### nitin
+# 
+
+# ***nitin***
+
+# * Google site https://www.google.com/
+
+# *** point 1 ***
+
+# ### Python Basics
+# * Python version 3.7
+# * Functions Programming
+# * Object oriented Programming 
+# * Scripting Programming
+
+# In[11]:
+
+
+print("Hello,Gitam")
+print("Hyderabad")
+
+
+# In[16]:
+
+
+print("Hello, Gitam","|||",end =" ")
+print("Hyderabad")
+print("Python Programming","|||",end=" ")
+
+
+# In[19]:
+
+
+n1 = 100 #single
+a=b=c=20 #multi
+a1,b1,c1=10,20,30
+print(n1)
+print(a,b,c)
+print(a1,b1,c1)
+
+
+# In[23]:
+
+
+a=''
+s1=20
+print(a)
+print(s1) 
+print(type(a))
+print(type(s1))
+
+
+# In[4]:
+
+
+i = 100
+print(type(i))
+s1 = str(i) #str
+print(type(s1))
+f1 = float(i) #float
+print(type(f1))
+
+
+# In[5]:
+
+
+a1 = 1234
+print(len(str(a1)))
+
+
+# In[7]:
+
+
+s1 = input("Enter your name")
+print(s1)
+print(type(s1))
+           
+
+
+# In[8]:
+
+
+n1 = 1234
+print(n1+10)
+print(n1-10)
+print(n1*10)
+print(n1%10)
+print(n1//10)
+print(n1**10)
+
+
+# ### Precedence of the Arithematic Operators
+# * Parenthesis
+# * Power
+# * Multiplication
+# * Addition
+# 
+
+# In[9]:
+
+
+x = 1 + 2 ** 3/4 + 5
+print(x)
+
+
+# In[10]:
+
+
+x = 1+2 ** 3 / 4 * 5
+print(x)
+
+
+# In[12]:
+
+
+i = 100
+a1 = (i > 15) and (i < 800)
+a2 = (i > 150) and (i < 800)
+print(a1)
+print(a2)
+
+
+# In[1]:
+
+
+n = int(input("Enter a number"))
+if n%2 == 0:
+    print ("Even")
+else:
+    print ("odd") 
+
+
+# In[2]:
+
+
+n = int(input("enter a number"))
+if n%2 == 0:
+    print("even")
+else:
+    print("odd")
+
+
+# In[6]:
+
+
+n = int(input("enter a number"))
+if n == 0:
+       print("zero")
+elif n>0:    
+       print("positive")
+elif n<0:
+       print("Negative")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
